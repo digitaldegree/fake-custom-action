eeb3d33624dd9724dc6dfe56a9d5b106a1b9a82f
Michael <managedkaos@users.noreply.github.com>
2025-08-01 00:21:45 -0700

    GITHUB_EVENT_PATH

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Triggered by: $GITHUB_ACTOR"
 
 if [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
   echo "Checking commit: $GITHUB_SHA"
-  FILES=$(jq -r '[.commits[].added[], .commits[].modified[]] | unique[]' "$EVENT_PATH")
+  FILES=$(jq -r '[.commits[].added[], .commits[].modified[]] | unique[]' "$GITHUB_EVENT_PATH")
   echo "📄 Files changed in commit: $FILES"
 else
   echo "⏭️ Event '$GITHUB_EVENT_NAME' is not a push. Skipping file count."
