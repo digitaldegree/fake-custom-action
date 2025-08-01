f0835a10bc87e64b3b40f864dc2300b6a07f8fa3
Michael <managedkaos@users.noreply.github.com>
2025-08-01 00:29:14 -0700

    jq .

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Triggered by: $GITHUB_ACTOR"
 if [[ "$GITHUB_EVENT_NAME" == "push" ]]; then
   echo "Checking commit: $GITHUB_SHA"
   echo "EVENT JSON:"
-  jq "$GITHUB_EVENT_PATH"
+  jq . "$GITHUB_EVENT_PATH"
 else
   echo "Event '$GITHUB_EVENT_NAME' is not a push. Skipping file count."
 fi
